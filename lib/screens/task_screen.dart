696c79fc58642b1cebce887230e465664a89b346
kefeh <kefehcollins@gmail.com>
2021-07-27 06:50:20 +0100

    add functionality to the ontap of the FAB to navigate to the new add notes screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:note_takey/screens/add_task_screen.dart';
+import 'package:note_takey/widgets/search_form.dart';
+import 'package:note_takey/widgets/staggered_grid.dart';
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({Key? key}) : super(key: key);
+
+  Widget buildBottomModal(BuildContext context) {
+    FocusScope.of(context).requestFocus(FocusNode());
+    return Container();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +21,30 @@ class TasksScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: mainColor,
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddTaskScreen(),
+            ),
+          )
+        },
         backgroundColor: accentColorSecondary,
         child: const Icon(Icons.add),
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
               Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    padding: const EdgeInsets.only(top: 16.0),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const SizedBox(
+                      children: const [
+                        SizedBox(
                           width: 150,
                           child: Text(
                             "Hey milz,\nGood morning",
@@ -41,7 +55,7 @@ class TasksScreen extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const CircleAvatar(
+                        CircleAvatar(
                           backgroundColor: accentColor,
                           radius: 25.0,
                           child: Icon(
@@ -56,125 +70,22 @@ class TasksScreen extends StatelessWidget {
                   const SizedBox(
                     height: 20,
                   ),
-                  Form(
-                    child: Container(
-                      height: 40,
-                      decoration: const BoxDecoration(
-                        color: searchColor,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5),
-                        ),
-                      ),
-                      child: TextFormField(
-                        textAlignVertical: TextAlignVertical.bottom,
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.search,
-                            size: 30,
-                          ),
-                          fillColor: Colors.white,
-                          hintText: "Search",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(15),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+                  const SearchForm(searchColor: searchColor),
+                  const SizedBox(
+                    height: 20,
                   ),
                 ],
               ),
               const SizedBox(
-                height: 30,
+                height: 10,
               ),
-              Expanded(
+              const Expanded(
                 child: SizedBox(
                   child: StGridView(accentColor: accentColor),
                 ),
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class StGridView extends StatelessWidget {
-  const StGridView({
-    Key? key,
-    required this.accentColor,
-  }) : super(key: key);
-
-  final Color accentColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return StaggeredGridView.countBuilder(
-      crossAxisCount: 2,
-      crossAxisSpacing: 22,
-      mainAxisSpacing: 22,
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        return CardItem(accentColor: accentColor);
-      },
-      staggeredTileBuilder: (index) {
-        return const StaggeredTile.fit(1);
-      },
-    );
-  }
-}
-
-class CardItem extends StatelessWidget {
-  const CardItem({
-    Key? key,
-    required this.accentColor,
-  }) : super(key: key);
-
-  final Color accentColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: accentColor, borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                "Health",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            Text(
-              "We will create an imageList URL in a list and define it on a staggered gridview.",
-              style: TextStyle(
-                fontSize: 11,
-                fontWeight: FontWeight.w500,
-                height: 1.5,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 8.0),
-              child: Text(
-                "20th May",
-                style: TextStyle(
-                  fontSize: 9,
-                  fontWeight: FontWeight.w500,
-                  height: 1.5,
-                ),
-              ),
-            ),
-          ],
         ),
       ),
     );
